6046be6f548c788ebb51627a531ab3e9ae634323
Kasper Moskwiak <kasper.moskwiak@gmail.com>
2018-05-26 00:41:07 +0200

    tests and jsdocs

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-class Root {
-    constructor(options) {
-        return new TreeNode(options.pathLength, null, 0);
+class BufferId {
+    /**
+     * Creates first node of tree
+     * @param {Object}  options
+     * @param {Number}  options.idLength
+     * @param {Number}  options.indexRange
+     * @param {String}  options.idFormat 
+     */
+    constructor(_options) {
+        let options = {
+            idLength: _options.idLength,
+            indexRange: _options.indexRange || 256,
+            idFormat: _options.idFormat || 'array'
+        };
+
+        return new TreeNode(options, null);
     }
 }
 
 class TreeNode {
-    constructor(maxBranchLength, parent, index) {
-        this.children = new Children();
-        this.maxBranchLength = maxBranchLength;
+    /**
+     * Creates node in tree
+     * @param {Object} options length of buffer
+     * @param {TreeNode | null} parent parent node or null if node is root  
+     * @param {Number} index index of node 
+     */
+    constructor(options, parent, index) {
+        this.options = options;
+        this.children = new Children(options.indexRange);
+        this.idLength = options.idLength;
+
         if(parent) {
             this.path = parent.getPath().concat(index);
         } else {
             this.path = [];
         }
         
-        if(this.path.length === maxBranchLength) {
+        if(this.path.length === this.idLength) {
             this.last = true;
             this.full = true;
         } else {
@@ -23,26 +44,45 @@ class TreeNode {
         }
     }
 
+    /**
+     * Removes identifier from tree
+     * @param {Array | Buffer} fullPath identifier - can be buffer or array
+     */
     remove(fullPath) {
+        let _path = fullPath;
+        if(fullPath instanceof Buffer) {
+            _path = fullPath.toJSON().data;
+        }
+
         return this.children.remove(fullPath);
     }
 
+    /**
+     * Sets node to not full
+     */
     setNotFull() {
         this.full = false;
     }
 
+    /**
+     * Check if node is full
+     * @returns {Boolean} true if node is full
+     */
     isFull() {
         this.checkFull();
         return this.full;
     }
 
+    /**
+     * Check if node is full
+     */
     checkFull() {        
         if(this.last) { 
             this.full = true; 
             return;
         }
 
-        if(!this.children.isSizeReached()) {
+        if(!this.children.isIndexRangeReached()) {
             this.full = false;
             return;
         }
@@ -51,65 +91,107 @@ class TreeNode {
         this.children.forEach((child) => {
             full = full && child.isFull();
         });
-        this.full = full && this.children.isSizeReached();
+        this.full = full && this.children.isIndexRangeReached();
     }
 
+    /**
+     * Creates new identifier as array. Buffer id is accessible under asBuffer property
+     * @returns {Array} identifier as array 
+     */
     create() {
-        if(this.getPath().length === this.maxBranchLength) {
+        if(this.getPath().length === this.idLength) {
+            if(this.options.idFormat === 'buffer') {
+                return Buffer.from(this.getPath());
+            }
             return this.getPath();
         }
 
         let child = this.children.getNotFull();
 
         if(!child) {
-            child = this.children.add(this.maxBranchLength, this);
+            child = this.children.add(this.options, this);
         }
 
         return child.create();
     }
 
+    /**
+     * Returns node path
+     * @returns {Array} node path
+     */
     getPath() {
+        let _path = this.path.slice();
+        Object.defineProperty(_path, 'asBuffer', { get: function() {
+            return Buffer.from(this);
+        }});
         return this.path;
     }
 
+    /**
+     * Returns node index
+     * @returns {Number} node index
+     */
     getIndex() {
         return this.getPath()[this.getPath().length - 1];
     }
 }
 
 class Children {
-    constructor() {
-        this.size = 10;
+    /**
+     * Creates array-like children object
+     * @param {Number} indexRange range of indexes
+     */
+    constructor(indexRange) {
+        this.indexRange = indexRange;
         this.children = [];
-        this.sizeReached = false;
+        this.indexRangeReached = false;
     }
 
+    /**
+     * Calls callback for each child
+     * @param {Function} cb callback function 
+     */
     forEach(cb) {
         this.children.forEach(cb);
     }
 
+    /**
+     * Returns child with given index
+     * @param {Number} index index of child 
+     * @returns {TreeNode} child with given index
+     */
     find(index) {
         for(let i = 0; i < this.children.length; i++) {
             if(this.children[i].getIndex() === index) {
                 return this.children[i];
             }
         }
+        return null;
     }
 
+    /**
+     * Removes identifier from tree
+     * @param {Array} fullPath identifier to remove
+     * @returns {TreeNode | false} removed child node or false if child does not exist
+     */
     remove(fullPath) {
         let index = fullPath.shift();
         let child = this.find(index);
-        child.setNotFull();
 
+        if(!child) { return false; }
+        
+        child.setNotFull();
         if(fullPath.length) {
             return child.remove(fullPath);
         }
-
         this.children.splice(index, 1);
-
         return child;
     }
 
+    /**
+     * Return child which is not full
+     * @returns {TreeNode} not full child
+     */
     getNotFull() {
         for(let i = 0; i < this.children.length; i++) {
             let child = this.children[i];
@@ -121,21 +203,35 @@ class Children {
         return null;
     }
 
-    isSizeReached() {
-        this.sizeReached = this.children.length === this.size;
-        return this.sizeReached;
+    /**
+     * Checks if range og childres is reached
+     * @returns {Boolean} true if range of children is reached
+     */
+    isIndexRangeReached() {
+        this.indexRangeReached = this.children.length === this.indexRange;
+        return this.indexRangeReached;
     }
 
-    add(maxBranchLength, parent) {
-        if(this.isSizeReached()) { return false; }
+    /**
+     * Creates new child in set of children
+     * @param {Object} options options passed to root node
+     * @param {TreeNode} parent node of parent
+     * @returns {TreeNode} added child 
+     */
+    add(options, parent) {
+        if(this.isIndexRangeReached()) { return false; }
         let index = this.getFreeIndex();
-        let child = new TreeNode(maxBranchLength, parent, index);
+        let child = new TreeNode(options, parent, index);
         this.children.splice(index, 0, child);
         return child;
     }
 
+    /**
+     * Returns available index
+     * @returns {Number} available index
+     */
     getFreeIndex() {
-        for(let i = 0;  i < this.size; i++) {
+        for(let i = 0;  i < this.indexRange; i++) {
             if(!this.children[i] || this.children[i].getIndex() !== i) {
                 return i;
             }
@@ -143,4 +239,4 @@ class Children {
     }
 }
 
-module.exports = Root;
+module.exports = BufferId;
--- tests/test.js
+++ tests/test.js
@@ -1,31 +1,89 @@
 import test from 'ava';
-import Tree from '../index';
+import BufferId from '../index';
 
 test('First path should be [0,0,0]', t => {
-    const root = new Tree({
-        pathLength: 3
+    const root = new BufferId({
+        idLength: 3,
+        indexRange: 10
     });
+
     let path = root.create();
     t.deepEqual(path, [0,0,0]);
 });
 
 test('Third path should be [0,0,2]', t => {
-    const root = new Tree({
-        pathLength: 3
+    const root = new BufferId({
+        idLength: 3,
+        indexRange: 10
     });
+
     root.create();
     root.create();
     let path = root.create();
     t.deepEqual(path, [0,0,2]);
 });
 
-test('Path removal should release it', t => {
-    const root = new Tree({
-        pathLength: 3
+test('ID removal should release it', t => {
+    const root = new BufferId({
+        idLength: 3,
+        indexRange: 10
     });
+
     root.create();
     root.create();
     root.remove([0,0,1]);
     let path = root.create();
     t.deepEqual(path, [0,0,1]);
+});
+
+test('ID should have length 2', t => {
+    const root = new BufferId({
+        idLength: 2,
+        indexRange: 10
+    });
+
+    let path = root.create();
+    t.is(path.length, 2);
+});
+
+test('Last ID should be [1,1]', t => {
+    const root = new BufferId({
+        idLength: 2,
+        indexRange: 5
+    });
+
+    root.create();
+    root.create();
+    root.create();
+    root.create();
+    root.create();
+    root.create();
+
+    let path = root.create();
+    t.deepEqual(path, [1,1]);
+});
+
+test('indexRange should be 256 if option is not specified', t => {
+    const root = new BufferId({
+        idLength: 3
+    });
+
+    for(let i = 0; i < 255; i++) {
+        root.create();
+    }
+
+    let path = root.create();
+    let nextPath = root.create();
+    t.deepEqual(path, [0,0,255]);
+    t.deepEqual(nextPath, [0,1,0])
+});
+
+test('id should be buffer if format is set to buffer', t => {
+    const root = new BufferId({
+        idLength: 3,
+        idFormat: 'buffer'
+    });
+
+    let path = root.create();
+    t.is(path instanceof Buffer, true);
 });
